746484067d6403c611dd19a1906d0bf925b5e0a0
Jesus Cuza <Jcuza@knights.ucf.edu>
2016-08-26 17:59:33 -0400

    Second iteration of homework

--- liri.js
+++ liri.js
@@ -1,108 +1,114 @@
-//Take user input for action
+// ACCEPT UNTRUSTED CERTIFICATES
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+
+//TAKE USER INPUT FOR ACTION
 var action = process.argv[2];
 
-//User input for movie-this and spotify-this-song
+//USER INPUT FOR MOVIE-THIS AND SPOTIFITY-THIS-SONG
 var parameter = process.argv[3]
-// .split(" ").join('+');
 
-//Switch to determine action to take
-switch(action) {
-	case 'my-tweets':
-		twitter();
-		break;
-	case 'spotify-this-song':
-		spotify();
-		break;
-	case 'movie-this':
-		movie();
-		break;
-	case 'do-what-it-says':
-		doIt();
-		break;
-	default:
-		console.log("Enter 'my-tweets', 'spotify-this-song', 'movie-this', or 'do-what-it-says'");
+//SWITCH TO DETERMINE ACTION TO TAKE
+function doAction(action) {
+    switch(action) {
+        case 'my-tweets':
+            twitter();
+            break;
+        case 'spotify-this-song':
+            spotify();
+            break;
+        case 'movie-this':
+            movie();
+            break;
+        case 'do-what-it-says':
+            doIt();
+            break;
+        default:
+            console.log("Enter 'my-tweets', 'spotify-this-song', 'movie-this', or 'do-what-it-says'");
+    }
 }
 
-//my-tweets
+//MY-TWEETS FUNCTIONALITY
 function twitter() {
 
-	//Get twitter node package
+	//GET TWITTER NODE PACKAGE
 	var twitter = require('twitter');
 
-	//Grab the keys.js info
+	//UTILIZE KEY.JS INFORMATION
 	var twitterKeys = require('./keys.js').twitterKeys;
 
-	//Set client to the grabbed key
+	//SET CLIENT TO THE GRABBED KEY
 	var client = new twitter(twitterKeys);
 
-	//Set screen_name and number of tweets to pull
-	var params = {screen_name: '@seancapelle', count: 10};
+	//SET SCREEN NAME AND NUMBER OF TWEETS TO PULL 
+	var params = {screen_name: '@Sendego', count: 20};
 
-	//Get timeline info
+	//GET TIMELINE INFO
 	client.get('statuses/user_timeline', params, function(error, tweets) {
 	 	
-	 	//If error occurs
+	 	//IF AN ERROR HAPPENS...
 	 	if (error) {
+
+	 		// RETURN THE FOLLOWING
 		    console.log('Error occurred: ' + error);
 		    return;
 		}
 	 	
-	 	//If no error
+	 	//IF NO ERROR
 	 	if (!error) {
 		
-			//Display ten current tweets, numbered 1-10
+			//DISPLAY 20 CURRENT TWEETS, NUMBER 1-20
 			for (var i = 0; i < tweets.length; i++) {
-				console.log((parseInt([i]) + 1) + '. ' + tweets[i].text);
+				console.log((parseInt([i]) + 1) + '. ' + tweets[i].created_at + ' - ' + tweets[i].text);
 			}    
 		}
 	});
 
 }
-//spotify-this-song
+//SPOTIFY FUNCTIONALITY
 function spotify() {
 
 	console.log("In Spotify");
-	// console.log(parameter);
 
-	//Get spotify node package
+	//RETRIEVE SPOTIFY NODE PACKAGE
 	var spotify = require('spotify');
 	 
-	spotify.search({ type: 'track', query: parameter}, function(error, data) {
-	    //If error occurs
+	spotify.search({ type: 'track', query: parameter || 'ace of base the sign'}, function(error, data) {
+
+	    //IF ERROR OCCURS
 	    if (error) {
+
+	    	// RETURN THE FOLLOWING
 	        console.log('Error occurred: ' + error);
 	        return;
 	    }
 	 	
-	 	//If no error
+	 	//IF NO ERROR
 		if (!error) {
-			
-			//Artist name 
-			console.log(data.tracks.items[0].artists[0].name);
-			//Album name
-			console.log(data.tracks.items[0].album.name);
-			
-			console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-			console.log(data.tracks.items[0].album);
+            
+            // RETURN THE FOLLOWING INFORMATION
+            info = {
+                artists: data.tracks.items[0].artists.map(function (val) { return val.name; }).join(', '),
+                songName: data.tracks.items[0].name,
+                previewUrl: data.tracks.items[0].preview_url,
+                album: data.tracks.items[0].album.name
+            }
+            
 
-			// Artist(s)
-			// The song's name
-			// A preview link of the song from Spotify
-			// The album that the song is from
-			// if no song is provided then your program will default to
-
-			// "The Sign" by Ace of Base
+			console.log('Artist(s): ' + info.artists);
+			console.log('Album: ' + info.album);			
+			console.log('Name: ' + info.songName);
+			console.log('Preview URL: ' + info.previewUrl);
 
 		}
 	});
 
 }
-//movie-this
+//MOVIE FUNCTIONALITY
 function movie() {
 
 	console.log("In Movie");
 
-	//Get request package
+	//GET MOVIE PACKAGE
 	var request = require('request');
 
 	if(parameter == null) {
@@ -116,72 +122,48 @@ function movie() {
 
 	console.log(movieName);
 
-	//Create URL based on movieName
+	//CREATE URL BASED ON MOVIE NAME
 	var queryUrl = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&tomatoes=true&r=json';
 
 	console.log(queryUrl);
 
 	request(queryUrl, function (error, response, data) {
 
-		//If error occurs
+		//IF ERROR OCCURS 
 		 if (error) {
+
+		 	// RETURN THE FOLLOWING
 	        console.log('Error occurred: ' + error);
 	        return;
 	    }
-		//If no error
+		//IF NO ERROR
 		if (!error && response.statusCode == 200) {
 
-			//Parse the data
+			//PARSE THE DATA AND RETURN THE FOLLOWING CONSOLE.LOG
 			var grabData = JSON.parse(data);
 			console.log(grabData.Title + " (" + grabData.Year + "), Rated: " + grabData.Rated + ". Filmed in: " + grabData.Country + ". Language: " + grabData.Language + 
 				". Plot: " + grabData.Plot + " Starring: " + grabData.Actors + ". Rotten Tomatoes Rating: " + grabData.tomatoUserMeter + ", Rotten Tomatoes URL: " + grabData.tomatoURL);
 		}
 	});
-//ADD + BETWEEN WORDS IN MOVIE TITLE!!!
 }
 
-//do-what-it-says
+//DO WHAT IT SAYS FUNCTIONALITY
 function doIt() {
 
 	console.log("In doIt");
 
 	var fs = require('fs');
 
-	//Stores the contents of the reading inside the var "data"
-	fs.readFile("random.txt", "utf8", function(error, data) {
-
-	//Print the contents of data
-	console.log(data);
-
-	//TURN INTO ACTION
-
-	})
-
+	//STORES THE CONTENTS OF THE READING INSIDE THE VAR "DATA" 
+	fs.readFile("random.txt", "utf8", function(error, data) {        
+        var split = data.split(',')
+        
+        action = split[0];
+        parameter = split[1];
+        
+        doAction(action);
+	});
 }
 
 
-//NOTES
-//Use Case for the requests on step 5
-
-
-// function movieThis(){
-// 	var url = '';
-
-// 	request(url, function(error, response, body)) {
-
-// 		if(!error && response.statusCode == 200) {
-// 			console.log("Movie");
-// 			JSON.parse
-// 		}
-// 	}
-// }
-
-// INSTRUCTIONS:
-// Level 1: Take any movie with a word title (ex: Cinderella) as a Node argument and retrieve the year it was created
-// Level 2 (More Challenging): Take a move with multiple words (ex: Forrest Gump) as a Node argument and retrive the year it was created. 
-// ----------------------------------------------------------------------------------
-
-// ... are the places where you need to code!
-
-// Include the request npm package (Don't forget to run "npm install request" in this folder first!)
-// ...
+doAction(action);
